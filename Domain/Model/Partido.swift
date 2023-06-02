5964bc8058ebf152b5c47fe1cd0033c50901ec97
manoelfilho <manoel.tsf@gmail.com>
2023-06-01 21:34:47 -0300

    correcting name of idPartido in Partido Model

--- Domain/Model/Partido.swift
+++ Domain/Model/Partido.swift
@@ -3,19 +3,19 @@ import Foundation
 public struct Partido: Model {
     
     //MARK: Properties
-    let id: Int?
+    let idPartido: Int?
     let sigla: String?
     let nome: String?
     
     //MARK: Init
     public init(id: Int?, sigla: String?, nome: String?){
-        self.id = id
+        self.idPartido = id
         self.sigla = sigla
         self.nome = nome
     }
     
     private enum CodingKeys: String, CodingKey {
-        case id
+        case idPartido
         case sigla
         case nome
     }
